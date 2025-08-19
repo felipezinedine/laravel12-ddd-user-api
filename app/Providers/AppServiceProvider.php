6d8a28cb6ff0a94e480c40a6e78c37d8c422ec42
Felipe Zinedine <felipe@ligdoctor.com.br>
2025-08-19 10:28:07 -0300

    feat: install and config Laravel Passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonInterval;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::ignoreRoutes();
+        Schema::defaultStringLength(191);
     }
 }
