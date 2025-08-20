6b58eccc3036ebed362b91037a57113499654d79
Felipe Zinedine <felipe@ligdoctor.com.br>
2025-08-20 11:57:55 -0300

    chore: register user routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\{AuthController, UserController};
 
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => 'auth:api', 'prefix' => 'users'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('/store', [UserController::class, 'store']);
+    Route::post('/update/{id}', [UserController::class, 'update']);
+    Route::get('/delete/{id}', [UserController::class, 'delete']);
+});
